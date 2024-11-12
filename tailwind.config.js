fab4d75296ecdf05441a6b312706e22ea83ad7ca
hausenlot <petrehausen@gmail.com>
2024-11-12 15:10:41 +0800

    Trying to come up with fix with json styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    './**/*.{html,js}',
+    './data/**/*.json'
+  ],
+  safelist: [
+    'bg-blue-500',
+    'text-white',
+    'px-4',
+    'py-2',
+    'rounded',
+    'mt-2',
+  ],
   theme: {
     extend: {},
   },
