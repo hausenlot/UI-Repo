be2f5d91ae24365b7229b6da573d7ac1db538c77
hausenlot <hausenlot@users.noreply.github.com>
2024-11-13 08:24:27 +0000

    deploy: 0cdd0c1978de9b005994d8a89ee77840863d8867

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,41 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './**/*.{html,js}',
-    './data/**/*.json'
-  ],
-  safelist: [
-    'bg-blue-500',
-    'text-white',
-    'px-4',
-    'py-2',
-    'rounded',
-    'mt-2',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  module.exports = {
+    darkMode: 'class',
+    content: [
+      './index.html',
+      './*.js',
+      './data/**/*.json'
+    ],
+    safelist: [
+      'bg-blue-500',
+      'text-white',
+      'px-4',
+      'py-2',
+      'rounded',
+      'mt-2',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          dark: {
+            background: '#1a1a1a',
+            secondaryBackground: '#282828',
+            primary: '#a3a3a3',
+            secondary: '#666666',
+            accent: '#3d3d3d',
+            border: '#333333',
+            highlight: '#ffffff',
+          },
+          light: {
+            background: '#ffffff',
+            primary: '#333333',
+            secondary: '#555555',
+            accent: '#e5e5e5',
+            border: '#dddddd',
+            highlight: '#000000',
+          },
+        },
+      },
+    },
+    plugins: [],
 }
